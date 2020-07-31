5b3a546cfd86a76893bf45dc08795ce0b84656e6
ChinaDragon <a5246211@gmail.com>
2020-07-31 17:49:49 +0800

    new projiect

---
+++ src/main/java/com/chen/gitxuexi/Hello.java
@@ -0,0 +1,11 @@
+package com.chen.gitxuexi;
+
+import java.util.HashMap;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWord!");
+    }
+
+}
